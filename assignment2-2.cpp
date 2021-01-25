094128d3a8f41a81ffb998e0a58084ff9ad671a2
zt6801 <zxtang0002@zonemail.clpccd.edu>
2021-01-25 23:04:44 +0000

    assignment2-2.cpp

---
+++ assignment2-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    char letter;
+
+    letter = 90;
+    cout << letter ;
+    letter = 72;
+    cout << letter ;
+    letter = 73;
+    cout << letter ;
+    letter = 81;
+    cout << letter ;
+    letter = 73;
+    cout << letter << endl;
+
+}
